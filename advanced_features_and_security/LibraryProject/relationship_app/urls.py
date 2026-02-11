bfeadd79147500f3798dd4f386913bc1b1e94b56
Ernest Solomon <marveldad2@gmail.com>
2026-02-11 01:05:37 +0100

    Implementing a Custom User Model in Django

---
+++ advanced_features_and_security/LibraryProject/relationship_app/urls.py
@@ -0,0 +1,28 @@
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # Book & library
+    path('books/', views.list_books, name='list_books'),
+    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
+
+    # Authentication
+    path('login/', views.UserLoginView.as_view(), name='login'),
+    path('logout/', views.UserLogoutView.as_view(), name='logout'),
+    path('register/', views.register, name='register'),
+
+    # Role-based views
+    path('admin/', views.admin_view, name='admin_view'),
+    path('librarian/', views.librarian_view, name='librarian_view'),
+    path('member/', views.member_view, name='member_view'),
+
+    # **Permission-based book actions**
+    path("books/add/", views.add_book, name="add_book/"),      # must exist
+    path("books/edit/<int:pk>/", views.edit_book, name="edit_book/"),  # must exist
+    path('books/delete/<int:pk>/', views.delete_book, name='delete_book'),  # must exist
+]
+
+
+
+
